e30e58916b1c9088948e0d19f930dd78e65aad2d
Burmagin <byrmaginsasha@gmail.com>
2023-02-22 19:39:05 +0300

    Домашка №4

---
+++ Desktop/sem4_HomeTask/task25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа 
+//(A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double Numbers(string message)
+{
+Console.Write(message);
+double result = Convert.ToDouble(Console.ReadLine());
+return result;
+}
+double number = Numbers("Введите число: ");
+double degree = Numbers("Введите степень: ");
+
+double end = Math.Pow(number, degree);
+Console.WriteLine($"Получилось- {end}");
+
+
