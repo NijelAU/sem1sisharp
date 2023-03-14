5b03e5b491b54b3891517fa8f7f446d9797eef8a
Burmagin <byrmaginsasha@gmail.com>
2023-03-14 16:16:06 +0300

    sem 9

---
+++ task63/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// // N = 6 -> "1, 2, 3, 4, 5
+void RoudToN(int N)
+{
+    if (N == 0)
+    {
+        return;
+    }
+    RoudToN(N-1);
+    System.Console.Write($"{N} ");
+}
+
+
+System.Console.Write("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+RoudToN(N);
